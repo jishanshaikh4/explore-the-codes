767882d95c70fabe89c335b012ed93fd8fc264c8
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-07-11 19:30:26 +0530

    Added Absolute_Permutation.py

---
+++ Programming/Python/Miscellaneous/Absolute_Permutation.py
@@ -0,0 +1,25 @@
+// PROBLEM NAME: Absolute Permutation (Implementation) at HackerRank Copyright.
+// SUBMITTED BY: Jishan Shaikh
+// LEVEL: Intermediate in Python
+// =============================
+// What to learn?
+// map() and its features to problem solving.
+// ==========================================
+
+t = int(raw_input())
+ans = []
+
+for _ in xrange(t):
+    n,k = map(int,raw_input().split())
+    ans = [0 for _ in xrange(n)]
+    for i in xrange(1,n+1):
+        if i-k >0 and ans[i-k-1]==0:
+            ans[i-k-1] = i
+        elif i+k <= n and ans[i+k-1] == 0:
+            ans[i+k-1] = i
+        else:
+            break
+    if 0 in ans:
+        print -1
+    else:
+        print " ".join(map(str,ans))
